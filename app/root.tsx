650ccee6962404fbf0e0ecd7f7ae5b0591c2afcd
Alex Page <hi@alexpage.dev>
2023-03-07 21:49:52 +1100

    Remove iOS zoom on text input

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export function links() {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "jamzone.today",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1,maximum-scale=1",
 });
 
 export default function Root() {
