e028010d93eb6bb231f3b9a92ea8f4042a040bdb
Bismay Purkayastha <bismaypurkayastha5467@gmail.com>
2023-03-20 01:49:33 +0530

    Fixed bug in getTzOffset() (#11)
    * Fixed bug in getTzOffset()
    
    * Bug Fixed : Considered cases for negative offset values in getTzOffset()

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -40,7 +40,11 @@ function getTzOffset(timeZone: string) {
     timeZone,
   });
 
-  return Number(formatter.format(new Date()).split("GMT")[1]);
+  const offset = formatter.format(new Date()).split("GMT")[1];
+  const [hour = 0, minute = 0, second = 0] = offset.split(":").map(x => Number(x));
+
+  const offsetHour = Math.abs(hour) + minute/60 + second/3600;
+  return hour < 0 ? offsetHour : -offsetHour;
 }
 
 function getLocalTime(time: string, tz: string, localTz: string) {
